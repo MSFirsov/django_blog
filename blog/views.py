578c2d5efa7ef78bdc8cd043994c3f3349bb6286
MSFirsov <mxfrsv@gmail.com>
2025-04-09 21:59:15 +0300

    added html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    p_list = Post.objects.all()
+    data = {
+        'p_list': p_list
+    }
+    return render(request, 'blog/post_list.html', context=data)
