fcebeeff41ea3a73527cde76df8c0ebe0047e779
MSFirsov <mxfrsv@gmail.com>
2025-04-10 21:51:51 +0300

    create/edit posts

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 
 def post_list(request):
-    p_list = Post.objects.all()
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     data = {
-        'p_list': p_list
+        'posts': posts,
     }
     return render(request, 'blog/post_list.html', context=data)
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    data = {
+        'post': post,
+    }
+    return render(request, 'blog/post_detail.html', context=data)
+
+@login_required
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
